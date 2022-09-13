9acd042f23affa4e19a7d34b00fb2037d0a16acd
Benedek Dér <benedekder@gmail.com>
2022-09-13 16:33:09 +0200

    created sh file to automate configuration

--- k8s/automate.sh
+++ k8s/automate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-activeJobs=$1
-threads=$2
-sleepTime=$3
-sed -i "11 s/[0-9]+/$1/g" configmap.yaml
-sed -i "12 s/[0-9]+/$2/g" configmap.yaml
-sed -i "13 s/[0-9]+/$3/g" configmap.yaml
+#activeJobs: $1
+#threads: $2
+#sleepTime: $3
+sed -i -E "11 s/[0-9]+/$1/g" configmap.yaml
+sed -i -E "12 s/[0-9]+/$2/g" configmap.yaml
+sed -i -E "13 s/[0-9]+/$3/g" configmap.yaml
+
+#updates configmap, restarts deployment to apply changes
+kubectl apply -f configmap.yaml
+kubectl rollout restart deployment c8-perftest -n camunda-perf
